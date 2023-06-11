c7faef10bcbfb15ed35d6acc526c6308a947f515
Ferlony <Hegecioum@gmail.com>
2023-06-12 00:39:47 +0500

    base

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,10 +1,16 @@
 import asyncio
 import uvicorn
 from router import app
+import configparser
 
 
 async def main():
-    config = uvicorn.Config("main:app", port=8000, log_level="info")
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    config_host = str(config["DEFAULT"]["host"])
+    config_port = int(config["DEFAULT"]["port"])
+
+    config = uvicorn.Config("main:app", host=config_host, port=config_port, log_level="info")
     server = uvicorn.Server(config)
     await server.serve()
 
--- src/main/python/router.py
+++ src/main/python/router.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request
 
 
 app = FastAPI()
 front_path = "../resources/mediaservice/src/"
 
+app.mount(
+    "/static",
+    StaticFiles(directory=front_path + "styles"),
+    name="static",
+)
+
+templates = Jinja2Templates(directory=front_path + "templates")
+
 
 @app.get("/")
-async def get_index():
-    return FileResponse(front_path + "index.html")
+async def root(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request}
+    )
