ccf4bdfaefba1dcd2bede9314762c333a23d18b2
Ferlony <hegecioum@gmail.com>
2024-01-05 20:33:51 +0500

    pages for pics

---
+++ src/main/resources/mediaservice/src/templates/js/pictures.js
@@ -0,0 +1,58 @@
+let picList = [];
+const PIC_STEP = 30;
+var pics_counter = 0
+
+
+function __addNextPics() {
+    var elem = document.getElementById("picblock");
+    for (pics_counter; pics_counter < PIC_STEP; pics_counter++) {
+        let name = picList[pics_counter]["name"];
+        let src = picList[pics_counter]["src"];
+        var html_to_insert = (' \
+        <li> \
+            <a href="' + src + '" target="_blank"> \
+            <img class="img-fit" src="' + src + '" \
+                alt="Cant load: ' + name + '" /> \
+            </a> \
+        </li> \
+        ');
+        elem.insertAdjacentHTML('beforeend', html_to_insert);
+    }
+
+}
+
+
+function addNextPics() {
+    if ((PIC_STEP < picList.length) && (pics_counter < picList.length) && (pics_counter < PIC_STEP)) {
+        __addNextPics();
+    }
+    else {
+        addAllPics();
+    }
+}
+
+
+function addAllPics() {
+    var elem = document.getElementById("picblock");
+    for (pics_counter; pics_counter < picList.length; pics_counter++) {
+        let name = picList[pics_counter]["name"];
+        let src = picList[pics_counter]["src"];
+        var html_to_insert = (' \
+        <li> \
+            <a href="' + src + '" target="_blank"> \
+            <img class="img-fit" src="' + src + '" \
+                alt="Cant load: ' + name + '" /> \
+            </a> \
+        </li> \
+        ');
+        elem.insertAdjacentHTML('beforeend', html_to_insert);
+    }
+
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    console.log(picList);
+    addNextPics();
+
+});
